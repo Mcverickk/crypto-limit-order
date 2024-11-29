c55cdfdf2e785eef56c4dd6b606e44b464fd2aaf
Mcverickk <chiragagarwal2001@gmail.com>
2024-11-29 06:35:01 +0530

    Update order added

--- src/triggerTxn.js
+++ src/triggerTxn.js
@@ -1,9 +1,26 @@
-const { log } = require("./utils");
+const { log, postRequest } = require("./utils");
 
 
 const triggerTxn = async ({uniqueId, order, priceData}) => {
     log({ uniqueId, message: `==========LIMIT PRICE REACHED==========` });
     log({ uniqueId, message: `Triggering ${priceData.pairName} txn for id ${order.id} at price ${priceData.price}` });
+
+    const body = {
+        id: order.id,
+        status: "COMPLETED",
+        dex: priceData.dex,
+        txnHash: priceData.price.toString(),
+    };
+
+    const { response, error } = await postRequest({
+        uniqueId,
+        url: config.HASURA_UPDATE_ORDER_URL,
+        reqBody: body
+    });
+
+    if(error || !response?.update_LimitOrder_by_pk){
+        log({ uniqueId, message: "Error updating order", error });
+    }
 }
 
 
